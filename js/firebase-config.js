02e8e5b5731743d634228bcb0df5588670a2a9f9
Brent Niemerski <brentcniemerski@gmail.com>
2026-06-20 12:20:34 -0400

    fix admin auth?

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Firebase web config for the browser/CDN SDK used by js/firebase-init.js.
+// Do not import from "firebase/app" here unless you are using a bundler.
+export const firebaseConfig = {
   apiKey: "AIzaSyC98nPNl5j2qKlL_i26XJWZcPpabmW7Aow",
   authDomain: "bn-fifa-wc-tracker.firebaseapp.com",
   projectId: "bn-fifa-wc-tracker",
   storageBucket: "bn-fifa-wc-tracker.firebasestorage.app",
   messagingSenderId: "697714939887",
-  appId: "1:697714939887:web:448b54e67a7fc39185c81a"
+  appId: "1:697714939887:web:448b54e67a7fc39185c81a",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
